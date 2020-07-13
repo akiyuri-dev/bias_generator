817abee6d7865027d58eff61954cf025ed62bfba
hinatakage <68187816+hinatakage@users.noreply.github.com>
2020-07-13 22:04:59 +0900

    修正

--- img.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if(isset($_POST['img'])){
-		//画像のダウンロード
-		header('Content-Type: application/octet-stream');
-		header('Content-disposition: attachment; filename="generator.png"');
-		echo base64_decode(str_replace('data:image/png;base64,','',$_POST['img']));
-	}
-?>
